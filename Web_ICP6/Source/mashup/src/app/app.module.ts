0280ed9861342a66ee04cb9eb251abd449530081
MounikaRachamadugu01 <mrnxq@mail.umkc.edu>
2020-06-26 22:21:08 -0500

    Create app.module.ts

---
+++ Web_ICP6/Source/mashup/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SearchRecipeComponent } from './search-recipe/search-recipe.component';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchRecipeComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
